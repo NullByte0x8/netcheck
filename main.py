2ef3df32a70f77a36a750ea44f57f7bda7b1a875
Gabor Korn <gabor@elan.rulez.org>
2026-02-25 12:25:37 +0100

    implement logging to stderr; optimise     -implement log() and switch to using it instead of print()     -implement debug variable, always set to True for now for dev     convenience     -remove some cleanup and error handling deemed to be unnecessary:             -remove quit()             -remove KeyboardInterrupt handling             -keep fifo open all the time     -output domain name along with ip

--- main.py
+++ main.py
@@ -21,15 +21,10 @@ icmpweight = 33
 totalweight = tcpweight + dnsweight + icmpweight
 highestweight = max(tcpweight, dnsweight, icmpweight)
 lowestweight = min(tcpweight, dnsweight, icmpweight)
+debug = True
+stdout = False
 
-try:
-    posix.mkfifo(pipe_path)
-    print("FIFO created")
-except FileExistsError:
-    print("FIFO exists, continue...")
-except OSError as e:
-    print(f"FIFO creation failed: {e}")
-    sys.exit(2)
+pipe = open(pipe_path, "w")
 
 def out(val):
     if(type(val) == int):
@@ -38,89 +33,87 @@ def out(val):
         raise TypeError("out(): val must be int")
         return False
     try:
-        with open(pipe_path, "w") as pipe:
-            pipe.write(val)
-            pipe.close
+        pipe.write(str(val))
+        pipe.close
     except OSError as e:
         raise Exception(f"out(): {e}")
         return False
 
-def quit(val, msg):
-    try:
-        os.remove(pipe_path)
-    except Exception as e:
-        print(e)
-    print(str(msg))
-    sys.exit(int(val))
+def log(msg):
+    print(f"{round(time.time(), 1)} netcheck: {msg}", file=sys.stderr)
+    if stdout: print(f"{time.time()} netcheck: {msg}")
 
 try:
-    opts, args = getopt.getopt(sys.argv, "hm:", ["help", "mark="])
+    posix.mkfifo(pipe_path)
+    if debug: log("FIFO created")
+except FileExistsError:
+    if debug: log("FIFO exists, continue...")
+    pass
+except OSError as e:
+    if debug: log(f"FIFO creation failed: {e}")
+    sys.exit(2)
+
+try:
+    opts, args = getopt.getopt(sys.argv, "hdm:", ["help", "mark="])
 except getopt.GetoptError:
-    print("netcheck.py -m:<SO_MARK value> | netcheck.py --mark=<SO_MARK value>")
+    print("netcheck.py [-d] [-h | -m:<SO_MARK value> | netcheck.py --mark=<SO_MARK value>]")
     sys.exit(2)
 for opt, arg in opts:
     if opt in ("-h", "--help"):
-        print("netcheck.py -m:<SO_MARK value> | netcheck.py --mark=<SO_MARK value>")
-        sys.exit()
-    elif opt in ("-m", "--mark"):
+        print("netcheck.py [-d] [-h | -m:<SO_MARK value> | netcheck.py --mark=<SO_MARK value>]")
+        sys.exit(0)
+    elif opt in ("-d", "--debug"):
+        debug = True
+        log(f"opts: {opts}; args: {args}")
+    if opt in ("-m", "--mark"):
         mark = arg
+        if debug: log(f"mark: {mark}")
 
 while True:
-    # moderandomizer = random.randint(1, 100)
-    # if moderandomizer <= 33:
+    # moderandomiser = random.randint(1, 100)
+    # if moderandomiser <= 33:
     #    mode = 1
-    # elif 33 < moderandomizer < 66:
+    # elif 33 < moderandomiser < 66:
     #    mode = 2
-    # elif moderandomizer >= 66:
+    # elif moderandomiser >= 66:
     #    mode = 3
     try:
         healthPercentage = successcount / 10 * 100
     except ZeroDivisionError:
         healthPercentage = 100
-    print("health %: " + str(healthPercentage) + "; success count:" + str(successcount))
-    try:
-        out(int(healthPercentage))
-    except KeyboardInterrupt:
-        quit(0, "\nexiting...")
-    except Exception as e:
-        quit(2, str(e))
+    log(f"health %: {str(healthPercentage)} ; success count: {str(successcount)}")
+    out(int(healthPercentage))
     if successcount >= 10:
         successcount -= 1
-    try:
         if mode == 1:  # TCP on port 80
             s = socket.socket()
             s.getsockopt(socket.SOL_SOCKET, socket.SO_MARK, )
             s.settimeout(0.5)
             host = random.choice(os.listdir(iplistdir))
+            with open(f"{iplistdir}/{host}") as f:
+                domain = f.read()
             port = 80
-            print("connecting to: " + host + " on port " + str(port))
+            log(f"connecting to: {host} ({domain.strip()}) on port {str(port)}")
             try:
                 s.connect((host, port))
             except OSError:
                 pass
             try:
                 if s.getpeername():
-                    print("connection successful")
+                    log("connection successful")
                     if successcount < 10:
                         successcount += 1
                         s.close()
                     else:
-                        print("connection unsuccessful")
+                        log("connection unsuccessful")
                         if successcount > 0:
                             successcount -= 1
                         s.close()
             except OSError:
-                print("connection unsuccessful")
+                log("connection unsuccessful")
                 if successcount > 0:
                     successcount -= 1
                     s.close()
         # add other modes
-    except KeyboardInterrupt:
-        quit(0, "\nexiting...")
-        sys.exit()
-    try:
-        time.sleep(0.5)
-    except KeyboardInterrupt:
-        quit(0, "\nexiting...")
-        sys.exit()
+    time.sleep(0.5)
 
