db14dfc83c427734aa1c54df51968e9e14afe46e
Gabor Korn <gabor@elan.rulez.org>
2026-03-03 20:35:32 +0100

    new main loop rework log()

--- main.py
+++ main.py
@@ -8,20 +8,19 @@ import os
 import optparse
 import posix
 import asyncio
+import statistics
 # import dns  # dnspython, pypi, 2.2.1
 
-iplistdir = "/run/netcheck/ips/latest"
-pipe_path = "/run/netcheck/pipe"
-successcount = 10
-healthPercentage = 100
-mode = 1  # 1 = TCP on port 80; 2 = DNS check; 3 = ICMP echo
-tcpweight = 33
-dnsweight = 33
-icmpweight = 33
-totalweight = tcpweight + dnsweight + icmpweight
-highestweight = max(tcpweight, dnsweight, icmpweight)
-lowestweight = min(tcpweight, dnsweight, icmpweight)
-debug = 2
+debug = None
+mark = ""
+iplistdir = ""
+pipe_path = ""
+tcp_timeout = 1
+tcp_port = 80
+tcp_amount = 10
+healthy_threshold = 90
+healthy_sleep = 5
+unhealthy_sleep = 0.5
 colors = (
         "\x1b[0m",   # end of color (DEBUG)
         "\x1b[93m",  # bright yellow (WARN)
@@ -34,7 +33,7 @@ def out(val):
     try:
         pipe.write(str(val) + "\n")
         pipe.close
-        log(0, f"wrote {val} to pipe")
+        log("dbg", f"wrote {val} to pipe")
     except OSError as e:
         raise Exception(f"out(): {e}")
         return False
@@ -42,80 +41,114 @@ def out(val):
 # log messages to stderr, and, if stdout flag is set, to stdout
 def log(sev, msg):
     match sev:
-        case 0:
+        case "dbg":
             if debug >= 2: print(f"{colors[0]}[DEBUG] {msg}", file=sys.stderr)
-        case 1:
+        case "wrn":
             if debug >= 1: print(f"{colors[1]}[WARN] {msg}", file=sys.stderr)
-        case 2:
+        case "err":
             print(f"{colors[2]}[ERROR] {msg}", file=sys.stderr)
 
 # process arguments
-parser=optparse.OptionParser()
-parser.add_option("-d", "--debug", dest="debug", metavar="VALUE", help="set logging level: 0 for errors only, 1 for errors and warnings, 2 for debug")
-parser.add_option("-m", "--mark", dest="mark", metavar="MARK", help="use so_mark MARK for packets")
-parser.add_option("-D", "--dir", dest="iplistdir", metavar="DIR", help="look for ip files in DIR")
-parser.add_option("-f", "--file", dest="pipe_path", metavar="FILE", help="use FIFO at FILE")
-(opts, args) = parser.parse_args()
+def parse_opts():
+    parser=optparse.OptionParser()
+    parser.add_option(
+            "-d", "--debug",
+            dest="debug",
+            metavar="VALUE",
+            type="int",
+            default=2,
+            help="set logging level: 0 for errors only, 1 for errors and warnings, 2 for debug")
+    parser.add_option(
+            "-m", "--mark",
+            dest="mark",
+            metavar="MARK",
+            help="use so_mark MARK for packets")
+    parser.add_option(
+            "-D", "--dir",
+            dest="iplistdir",
+            metavar="DIR",
+            default="/run/netcheck/ips/latest",
+            help="look for ip files in DIR")
+    parser.add_option(
+            "-f", "--file",
+            dest="pipe_path",
+            metavar="FILE",
+            default="/run/netcheck/pipe",
+            help="use FIFO at FILE")
+    (opts, args) = parser.parse_args()
 
-log(0, f"opts: {opts}; args: {args}")
+    global debug, mark, iplistdir, pipe_path
+    debug, mark, iplistdir, pipe_path = opts.debug, opts.mark, opts.iplistdir, opts.pipe_path
+
+    log("dbg", f"opts: {opts}; args: {args}")
 
 # create fifo
-try:
-    posix.mkfifo(pipe_path)
-    log(0, f"FIFO created at {pipe_path}")
-except FileExistsError:
-    log(0, f"Found FIFO at {pipe_path}")
-    pass
-except OSError as e:
-    log(2, f"FIFO creation failed at {pipe_path}: {e}")
-    sys.exit(2)
+def create_fifo():
+    try:
+        posix.mkfifo(pipe_path)
+        log("dbg", f"FIFO created at {pipe_path}")
+    except FileExistsError:
+        log("dbg", f"Found FIFO at {pipe_path}")
+    except OSError as e:
+        log("err", f"FIFO creation failed at {pipe_path}: {e}")
+        sys.exit(2)
 
-with open(pipe_path, "w", buffering=1) as pipe:
-    while True:
-        # moderandomiser = random.randint(1, 100)
-        # if moderandomiser <= 33:
-        #    mode = 1
-        # elif 33 < moderandomiser < 66:
-        #    mode = 2
-        # elif moderandomiser >= 66:
-        #    mode = 3
+# run a single tcp connection attempt
+async def try_tcp(host, port, hostname, timeout):
+    log("dbg", f"trying tcp for {host}, {hostname}")
+    async def inner():
         try:
-            healthPercentage = successcount / 10 * 100
-        except ZeroDivisionError:
-            healthPercentage = 100
-        log(0, f"health %: {str(healthPercentage)} ; success count: {str(successcount)}")
-        out(int(healthPercentage))
-        if successcount >= 10:
-            successcount -= 1
-        if mode == 1:  # TCP on port 80
-            s = socket.socket()
-            s.getsockopt(socket.SOL_SOCKET, socket.SO_MARK, )
-            s.settimeout(0.5)
-            host = random.choice(os.listdir(iplistdir))
-            with open(f"{iplistdir}/{host}") as f:
-                domain = f.read()
-            port = 80
-            log(0, f"connecting to: {host} ({domain.strip()}) on port {str(port)}")
-            try:
-                s.connect((host, port))
-            except OSError:
-               pass
-            try:
-                if s.getpeername():
-                    log(0, f"connection to {host} ({domain.strip()}) successful")
-                    if successcount < 10:
-                        successcount += 1
-                        s.close()
-                else:
-                    log(0, f"connection to {host} ({domain.strip()}) unsuccessful: !getpeername()")
-                    if successcount > 0:
-                        successcount -= 1
-                    s.close()
-            except OSError as e:
-                log(0, f"connection to {host} ({domain.strip()}) unsuccessful: {e}")
-                if successcount > 0:
-                    successcount -= 1
-                s.close()
-        # add other modes
-        time.sleep(0.5)
+            reader, writer = await asyncio.open_connection(host, port)
+            writer.close()
+            await writer.wait_closed()
+        except ConnectionRefusedError:
+            log("dbg", f"connection to {host}, {hostname} refused (success)")
+        except Exception as e:
+            log("dbg", f"connection to {host}, {hostname} failed: {e}")
+            return False
+        log("dbg", f"connection to {host}, {hostname} successful")
+        return True
+    try:
+        async with asyncio.timeout(timeout):
+            ret = await inner()
+    except TimeoutError:
+        log("dbg", f"connection to {host}, {hostname} failed")
+        return False
+    return ret
 
+# run try_tcp tcp_amount times concurrently
+async def run_tcp_test(timeout):
+    ip_list = random.sample(os.listdir(iplistdir), tcp_amount)
+    hn_list = []
+    success = 0
+    for filename in ip_list:
+        with open(f"{iplistdir}/{filename}", "r") as f:
+            hn = f"{f.read().strip()}"
+            hn_list.append(hn)
+    log("dbg", f"ip list for tcp test: {ip_list}")
+    log("dbg", f"hostname list for tcp test: {hn_list}")
+    results = await asyncio.gather(
+            *[try_tcp(ip_list[i], tcp_port, hn_list[i], timeout) for i in range(len(ip_list))])
+    log("dbg", f"results: {results}")
+    for r in results:
+        if r:
+            success += 1
+    log("dbg", f"successes in ip test: {success}")
+    return success
+
+async def main():
+    parse_opts()
+    with open(pipe_path, "w", buffering=1) as pipe:
+        create_fifo()
+        timeout = tcp_timeout
+        while True:
+            result = await run_tcp_test(timeout)
+            perc = int(result / tcp_amount * 100)
+            log("dbg", f"network health: {perc}")
+            if perc >= healthy_threshold:
+                time.sleep(healthy_sleep)
+            else:
+                time.sleep(unhealthy_sleep)
+            
+
+asyncio.run(main())
